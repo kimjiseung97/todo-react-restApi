d2d881f25622740c1a09ec6ba792b2dbfd7c0ecb
jisuengkim <dickseung23@naver.com>
2023-06-02 10:54:11 +0900

    # todoList db연동 rest api 구축

---
+++ src/main/java/com/example/todo/todoapi/dto/response/TodoDetailResponseDTO.java
@@ -0,0 +1,28 @@
+package com.example.todo.todoapi.dto.response;
+
+import com.example.todo.todoapi.entity.Todo;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoDetailResponseDTO {
+
+    private String id;
+
+    private String title;
+
+    private boolean done;
+
+    //엔터티를 dto로 변환하는 생성자
+
+    public TodoDetailResponseDTO(Todo todo){
+        this.id = todo.getTodoId();
+        this.title = todo.getTitle();
+        this.done = todo.isDone();
+    }
+}
